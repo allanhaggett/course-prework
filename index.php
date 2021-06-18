10a5bf156cbcd05f874a03e6bc862d82457858e1
Allan Haggett <allan.haggett@gov.bc.ca>
2021-06-17 17:31:43 -0700

    Better starting point but not documented yet

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php opcache_reset() ?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,31 +6,69 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="author" content="The Learning Centre">
 
-
-
 <meta name="description" content="">
 
-<title>Course Pre-Work Template</title>
+<title>Course Title - Course Pre-work</title>
 
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-
+<link rel="stylesheet" 
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" 
+      integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" 
+      crossorigin="anonymous">
+      
+<style>
+/** You can also use .img-fluid on images, but I almost never want a different
+    behaviour, so I set responsive images (and iframes) globally */
+iframe,
+img {
+  height: auto;
+  max-width: 100%;
+}
+</style>
 </head>
 <body>
 
-<nav class="site-header sticky-top py-1 bg-white">
+<nav class="site-header py-3 mb-3 text-white" style="background-color: #003366;">
   <div class="container d-flex flex-column flex-md-row justify-content-between">
-    <a class="py-2" href="#" aria-label="Product">
-      Learning Centre
-    </a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Tour</a>
+    <span class="navbar-brand d-inline-block mt-2" style="font-size: 1.6em;">Course Pre-Work</span>
+    <img alt="Where Ideas Work logo" class="d-none d-md-block" src="where-ideas-work.png" width="300">
   </div>
 </nav>
 
+<div class="container-fluid">
+<div class="row justify-content-md-center">
+<div class="col-md-12">
 
+</div>
+</div>
+</div>
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script>window.jQuery || document.write('<script src="/docs/4.6/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="/docs/4.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+<div class="container-fluid bg-light">
+<div class="row justify-content-md-center">
+<div class="col-md-6">
+<div class="bg-white my-5 p-5 text-center rounded-lg shadow-sm">
+<img alt="Where Ideas Work" src="wiw-pos-rgb.png" width="420">
+</div>
+</div>
+<div class="col-md-6">
+<div class="bg-white my-5 p-5 text-center rounded-lg shadow-sm">
+Brought to you by:<br>
+<a href="https://learningcentre.gww.gov.bc.ca/"
+    target="_blank" 
+    rel="noopener">
+      <img alt="Brought to you by the Learning Centre" height="100" src="lc-logo-wordmark-300x100.png" width="300">
+</a>
+</div>
+</div>
+</div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
+          integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" 
+          crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="/docs/4.6/assets/js/vendor/jquery.slim.min.js"><\/script>')</script>
+<script src="/docs/4.6/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" 
+        crossorigin="anonymous"></script>
 
 
 </body>
